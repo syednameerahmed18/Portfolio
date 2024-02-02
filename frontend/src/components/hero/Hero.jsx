49ed78ce1b15cf5d3a459a1e65ca9ef18bb9a6a8
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-02-02 15:38:08 +0530

    Update Hero.jsx

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -9,23 +9,39 @@ function Hero() {
         <motion.div 
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 1 }}>
+            transition={{ duration: 1, ease: "easeInOut" }}>
           <h1>Hi there,</h1>
+          
+        </motion.div>
+        <motion.div 
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1, ease: "easeInOut", delay:0.2 }}>
           <h2>I am Syed Nameer Ahmed,</h2>
+        </motion.div>
+        <motion.div 
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1, ease: "easeInOut", delay:0.4 }}>
           <h3>I am a Bangalore based Software Developer</h3>
         </motion.div>
         <motion.div
          className="Hero-Btn"
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 1.5 }}>
+            transition={{ duration: 1.5, ease: "easeInOut", delay:0.6 }}>
           <button>Let's Talk</button>
           <button>Download CV</button>
         </motion.div>        
       </div>
-      <div className="Hero-Picture">
+      <motion.div 
+        className="Hero-Picture"
+        initial={{opacity: 0, scale:0.5}}
+        animate={{opacity:1, scale:1}}
+        transition={{duration: 1, ease: "easeInOut"}}
+        >
         <img src={picture} alt="Syed Nameer Ahmed Picture"></img>
-      </div>
+      </motion.div>
     </div>
   )
 }
