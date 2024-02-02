2762674d99d17bae95fe02b3d2061927ba20808f
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-02-02 15:29:51 +0530

    Update Hero.jsx

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -1,17 +1,27 @@
 import "./Hero.css"
 import picture from '../../assets/syednameerahmed.jpg'
+import {motion} from "framer-motion"
 
 function Hero() {
   return (
     <div id="Hero" className="Hero-Container">
       <div className="Hero-Intro">
-        <h1>Hi there,</h1>
-        <h2>I am Syed Nameer Ahmed,</h2>
-        <h3>I am a Bangalore based Software Developer</h3>
-        <div className="Hero-Btn">
+        <motion.div 
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}>
+          <h1>Hi there,</h1>
+          <h2>I am Syed Nameer Ahmed,</h2>
+          <h3>I am a Bangalore based Software Developer</h3>
+        </motion.div>
+        <motion.div
+         className="Hero-Btn"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1.5 }}>
           <button>Let's Talk</button>
           <button>Download CV</button>
-        </div>        
+        </motion.div>        
       </div>
       <div className="Hero-Picture">
         <img src={picture} alt="Syed Nameer Ahmed Picture"></img>
