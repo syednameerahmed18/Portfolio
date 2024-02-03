e77e9d5f9e41236a9b5d7c9fdff93d8f68f8969c
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-02-03 15:23:00 +0530

    Project Section

--- frontend/src/components/projects/Projects.jsx
+++ frontend/src/components/projects/Projects.jsx
@@ -1,9 +1,21 @@
 import "./Projects.css" 
-
+import Ai from "../../assets/AiVoiceAssistant.jpg"
 function Projects() {
   return (
     <div id="projects" className="Project-Container">
         <div className="Project-Header">Projects</div>
+        <div className="Projects">
+          <div className="Project-1">
+            <img src={Ai} alt="Project Picture"></img>
+            <h1>AI Voice Assistant Using Python</h1>
+          </div>
+          <div className="Project-2">
+            Project 2
+          </div>
+          <div className="Project-3"> 
+            Project 3
+          </div>
+        </div>
     </div>
   )
 }
