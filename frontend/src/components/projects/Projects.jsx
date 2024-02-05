50f265cb9c0e271fbb7f22f0763c934592dfb9d1
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-02-05 19:27:16 +0530

    Added Projects

--- frontend/src/components/projects/Projects.jsx
+++ frontend/src/components/projects/Projects.jsx
@@ -1,13 +1,28 @@
 import "./Projects.css" 
+import { motion } from "framer-motion"
 import Ai from "../../assets/AiVoiceAssistant.jpg"
+
+
 function Projects() {
   return (
     <div id="projects" className="Project-Container">
-        <div className="Project-Header">Projects</div>
+        <motion.div 
+          className="Project-Header"
+          initial = {{opacity: 0, scale: 0.5}}
+          whileInView = {{opacity:1, scale:1}}
+          transition={{duration:1, ease: "easeInOut", delay:0.3}}
+          >
+          Projects
+        </motion.div>
         <div className="Projects">
           <div className="Project-1">
             <img src={Ai} alt="Project Picture"></img>
-            <h1>AI Voice Assistant Using Python</h1>
+            <div className="Project-1-Desc">
+              <a href="https://github.com/syednameerahmed18/Ai-voice-assistant/tree/main"><h1>AI Voice Assistant Using Python</h1></a>
+              <p>This Project was built using Python packages</p>
+              <p>like pyttsx3, speech_recognition, wikipedia</p>
+              <p>webbrowser, googlesearch</p>
+            </div>
           </div>
           <div className="Project-2">
             Project 2
