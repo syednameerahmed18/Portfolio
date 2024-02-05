4c98a097f594d59f5aee31da6a1b58e7f61b3dc9
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-02-05 19:38:28 +0530

    made few changes

--- frontend/src/components/projects/Projects.jsx
+++ frontend/src/components/projects/Projects.jsx
@@ -1,6 +1,7 @@
 import "./Projects.css" 
 import { motion } from "framer-motion"
 import Ai from "../../assets/AiVoiceAssistant.jpg"
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 
 function Projects() {
@@ -10,15 +11,23 @@ function Projects() {
           className="Project-Header"
           initial = {{opacity: 0, scale: 0.5}}
           whileInView = {{opacity:1, scale:1}}
-          transition={{duration:1, ease: "easeInOut", delay:0.3}}
+          transition={{duration:1, ease: "easeInOut"}}
           >
           Projects
         </motion.div>
-        <div className="Projects">
+        <motion.div 
+          className="Projects"
+          initial = {{opacity: 0, scale: 0.5}}
+          whileInView = {{opacity:1, scale:1}}
+          transition={{duration:1, ease: "easeInOut"}}>
           <div className="Project-1">
-            <img src={Ai} alt="Project Picture"></img>
+            <motion.img
+               initial = {{opacity: 0, scale: 0.5}}
+               whileInView = {{opacity:1, scale:1}}
+               transition={{duration:1, ease: "easeInOut", delay:0.2}} 
+              src={Ai} alt="Project Picture"></motion.img>
             <div className="Project-1-Desc">
-              <a href="https://github.com/syednameerahmed18/Ai-voice-assistant/tree/main"><h1>AI Voice Assistant Using Python</h1></a>
+              <a href="https://github.com/syednameerahmed18/Ai-voice-assistant/tree/main"><h1>AI Voice Assistant Using Python <FaExternalLinkAlt className="Project-1-link"/></h1></a>
               <p>This Project was built using Python packages</p>
               <p>like pyttsx3, speech_recognition, wikipedia</p>
               <p>webbrowser, googlesearch</p>
@@ -30,7 +39,7 @@ function Projects() {
           <div className="Project-3"> 
             Project 3
           </div>
-        </div>
+        </motion.div>
     </div>
   )
 }
