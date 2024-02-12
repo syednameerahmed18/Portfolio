dc5d2ff4a384f25f6852531701d0913317d35ce0
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-02-12 15:39:31 +0530

    added contact me

--- frontend/src/components/contactme/ContactMe.jsx
+++ frontend/src/components/contactme/ContactMe.jsx
@@ -1,8 +1,17 @@
-
+import "./ContactMe.css"
+import {motion} from "framer-motion"
 
 function ContactMe() {
   return (
-    <div id="contact">ContactMe</div>
+    <div id="contact" className="Contact-Container">
+      <motion.div
+        initial = {{opacity: 0, scale: 0.5}}
+        whileInView={{opacity:1, scale:1}}
+        transition={{duration:1, ease: "easeInOut"}} 
+        className="Contact-Header">
+        Contact Me
+      </motion.div>
+    </div>
   )
 }
 
