9dc89a6255660ecaebde3f1bda7007304e1091f5
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-02-12 16:19:41 +0530

    .

--- frontend/src/components/contactme/ContactMe.jsx
+++ frontend/src/components/contactme/ContactMe.jsx
@@ -1,6 +1,7 @@
 import "./ContactMe.css"
 import {motion} from "framer-motion"
 import { FaWhatsapp } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 function ContactMe() {
   // Function to Redirect to Whatsapp chat
@@ -8,6 +9,11 @@ function ContactMe() {
     const phoneNumber = "+918310782008"
     const whatsappURL = `https://wa.me/${phoneNumber}`
     window.open(whatsappURL, "_blank")
+  }
+
+  const handleLinkedIn = () => {
+    const LinkedInURL = "https://www.linkedin.com/in/syed-nameer-034378205/"
+    window.open(LinkedInURL, "_blank")
   }
 
   return (
@@ -26,7 +32,12 @@ function ContactMe() {
           transition={{duration:1, ease: "easeInOut", delay: 0.2}}>
             <FaWhatsapp  className="Wa-Icon" onClick={handleWhatsapp}/>
         </motion.div>
-        
+        <motion.div 
+          initial = {{opacity: 0, scale: 0.5}}
+          whileInView={{opacity:1, scale:1}}
+          transition={{duration:1, ease: "easeInOut", delay: 0.4}}>
+            <FaLinkedin  className="Li-Icon" onClick={handleLinkedIn}/>
+        </motion.div>
       </div>
     </div>
   )
