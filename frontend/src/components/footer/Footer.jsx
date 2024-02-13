d530afc71748dbef0f18212f696797fa19a63c33
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-02-13 19:40:03 +0530

    added footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
 import "./Footer.css"
+import { AiOutlineCopyright } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className="Footer-Container">Footer</div>
+    <div className="Footer-Container">
+      <div className="Copyright">
+        <p>Copyright</p>
+        <AiOutlineCopyright/>
+      </div>
+      <div className="Vertical-Bar"></div>
+      <div className="Footer-Items">
+        <div className="Footer-Content">
+          <p>Designed & Developed by</p>
+        </div>
+        <div className="Footer-Title">
+          <h1>Syed Nameer Ahmed</h1>
+        </div>
+      </div>
+      <div className="Vertical-Bar"></div>
+      <div className="Links">
+        <p>Follow me on</p>
+        <div className="Link-Icons">
+          <FaInstagram className="Insta"/>
+          <FaGithub className="Github"/>
+          <FaLinkedin className="LinkedIn"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
