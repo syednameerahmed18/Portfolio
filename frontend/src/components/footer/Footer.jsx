e07b299b92026f61ef82c2ed7d7c21e091ef7a45
syednameerahmed18 <84968568+syednameerahmed18@users.noreply.github.com>
2024-02-14 14:39:50 +0530

    Update Footer.jsx

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -5,6 +5,21 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
 function Footer() {
+  const handleInsta = () => {
+    const instaURL = "https://www.instagram.com/syed_nameer_ahmed/"
+    window.open(instaURL, "_blank")
+  }
+
+  const handleGit = () => {
+    const gitURL = "https://github.com/syednameerahmed18"
+    window.open(gitURL, "_blank")
+  }
+
+  const handleLinkedin = () => {
+    const linkedinURL = "https://www.linkedin.com/in/syed-nameer-034378205/"
+    window.open(linkedinURL, "_blank")
+  }
+
   return (
     <div className="Footer-Container">
       <div className="Left-Footer">
@@ -30,9 +45,9 @@ function Footer() {
       <div className="Links">
         <p>Follow me on: </p>
         <div className="Link-Icons">
-          <FaInstagram className="Insta"/>
-          <FaGithub className="Github"/>
-          <FaLinkedin className="LinkedIn-Footer"/>
+          <FaInstagram className="Insta" onClick={handleInsta}/>
+          <FaGithub className="Github" onClick={handleGit}/>
+          <FaLinkedin className="LinkedIn-Footer" onClick={handleLinkedin}/>
         </div>
       </div>
     </div>
